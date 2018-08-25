a7bf097db2cc7a27ee8058e2eafef2911c2af908
emcardoso <eltonm.cardoso@gmail.com>
2018-08-25 02:18:29 -0300

    m

--- APEGInterp.hs
+++ APEGInterp.hs
@@ -3,8 +3,7 @@ module APEGInterp where
 import AbstractSyntax
 import qualified Data.Map as M
 import Data.Either
-import Data.Map
-import Control.Monad.ST.State.Lazy
+import Control.Monad.State.Lazy
 
 type VEnv  = M.Map String Value 
 type TyEnv = M.Map String ([Type],[Type], TyRuleEnv) 
@@ -19,38 +18,48 @@ data Value = VStr String
            | VMap VEnv
            | VLan [ApegRule]
            | VMeta MAPeg
+           | Undefined
            deriving Show
 
 matchAll :: [Type] -> [Type] -> Bool
 matchAll xs ys = (and $ zipWith (==) xs ys) && ((length xs) == (length ys))
 
 
-inferType :: RuleEnv -> MExpr -> Type
-inferType re (MkStr _)    = TyStr
-inferType re (MkEmptyMap) = TyMap TyStr
-inferType re (MkVar v)    = maybe (re !? v) ()
+-- inferType :: TyRuleEnv -> MExpr -> Type
+-- inferType re (MkStr _)    = TyStr
+-- inferType re (MkEmptyMap) = TyMap TyStr
+-- inferType re (MkVar v)    = maybe (re !? v) ()
 
 
 unMeta :: MAPeg -> APeg
-unMeta MkLambda           = Right Lambda
-unMeta (MkCal nt inh syn) = NT (map unMetaE inh) (map unMetaE syn)
+unMeta MkLambda           = Lambda
+unMeta (MkCal nt inh syn) = NT nt (map unMetaE inh) (map unMetaE syn)
 unMeta (MkKle e)          = Kle (unMeta e)
 unMeta (MkNot e)          = Not (unMeta e)
 unMeta (MkSeq xs)         = Seq (map unMeta xs)
 unMeta (MkAlt xs)         = Alt (map unMeta xs)
-unMeta (MkAE xs)          = AEAttr (map (\(v,e) -> (v,unMetaE e)) xs)
+unMeta (MkAE xs)          = AEAttr $ map (\(v,e) -> (v,unMetaE e)) xs
 
-unMetaE :: MApeg -> Expr
+unMetaE :: MExpr -> Expr
 unMetaE (MkStr s)  = Str s
 unMetaE (MkEmptyMap) = EmptyMap
-unMetaE (MkVar v) = Var v
+unMetaE (MkVar v) = EVar v
 unMetaE (MkMp xs) = Mp (map (\(v,e) -> (v,unMetaE e)) xs)
-unMEtaE (MkMapIns eb s ee) = MapIns (unMetaE eb) s (unMetaE ee)
-unMetaE (MkMapAcces me ma ) = MkMapAcces (unMetaE me) (unMetaE ma) 
+unMetaE (MkMapIns eb s ee) = MapIns (unMetaE eb) s (unMetaE ee)
+unMetaE (MkMapAcces me ma ) = MapAcces (unMetaE me) (unMetaE ma) 
 
+
+var :: String -> APegSt Value
+var s = get >>= (\(grm,env,inp) -> case env M.!? s of 
+                                        Nothing -> fail "Undefined varibale"
+                                        Just v   -> return v)
+
+
+
+-- =================== Monad Utilityes =================== --
 
 evalExp :: Expr -> APegSt (Value)
-evalExp (Str s) = return s
-evalExp (EmptyMap s) = return s
-
+evalExp (Str s) = return (VStr s)
+evalExp (EmptyMap) = return (VMap M.empty)
+evalExp (EVar v)   = var v
 
